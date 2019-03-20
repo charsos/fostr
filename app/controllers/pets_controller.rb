19d4cd52ab1ab6bfc244e06d147050e8b3caf366
Derek Saffe <desaff@gmail.com>
2019-03-20 16:41:26 -0600

    added kano first attempt

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,7 +15,6 @@ class PetsController < ApplicationController
   end
 
   def create
-    raise
     @pet = Pet.new(pet_params)
     @pet.shelter = Shelter.find(params[:pet][:shelter_id])
     @pet.status = "available"
