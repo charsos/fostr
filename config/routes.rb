a64bfdb50621134b493c9e17006a69765b7705b0
Nicolas Delage <ndelage90@gmail.com>
2019-03-20 15:43:24 -0600

    added foster controller for new and create, plus form for new

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :pets, only: [:index, :show, :new, :create, :update, :edit] do
-    resources :fosters, only: [:create]
+    resources :fosters, only: [:new, :create]
   end
   resources :shelters, only: [:index, :show, :new, :create]
   # resources :users, only: [:show, :new, :create]
