d62420a914d612779d768092e2d7713992c25970
Derek Saffe <desaff@gmail.com>
2019-03-20 16:07:52 -0600

    fixed delete button

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :pets
   resources :shelters, only: [:index, :show, :new, :create]
   # resources :users, only: [:show, :new, :create]
-  delete 'pets/:id', to: 'pets#destroy', as: 'delete_pet'
+  # delete 'pets/:id', to: 'pets#destroy', as: 'delete_pet'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
