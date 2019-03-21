d49202d3d955483b2b1c1b91b9af2ec094ff257b
Amber Lowey <lowey45@gmail.com>
2019-03-21 12:56:58 -0600

    added three more data points/booleans to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_184609) do
+ActiveRecord::Schema.define(version: 2019_03_21_185454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 2019_03_21_184609) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "description"
+    t.boolean "good_with_kids"
+    t.boolean "good_with_other_pets"
+    t.boolean "hypoallergenic"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
