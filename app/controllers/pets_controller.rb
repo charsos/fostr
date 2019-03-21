f2d650bde984d6d63787d60fb425029a1b41587f
Houston CHarles <houstoncharles@outlook.com>
2019-03-21 09:58:15 -0600

    installed pundit, add admin column and policies for index, create, update, and destroy

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,9 @@
 class PetsController < ApplicationController
-  before_action :find_pet, only: [ :show ]
+  before_action :find_pet, only: [:show, :edit, :update, :destroy]
 
   def index
-    @pets = Pet.all
+    # @pets = Pet.all
+    @pets = policy_scope(Pet).order(created_at: :desc)
   end
 
   def show
@@ -11,6 +12,7 @@ class PetsController < ApplicationController
   def new
     @pet = Pet.new
     @my_shelters = Shelter.where(user: current_user.id)
+    authorize @pet
     # @shelter = Shelter.find(params[:shelter_id])
   end
 
@@ -18,6 +20,7 @@ class PetsController < ApplicationController
     @pet = Pet.new(pet_params)
     @pet.shelter = Shelter.find(params[:pet][:shelter_id])
     @pet.status = "available"
+    authorize @pet
     @pet.save
     redirect_to pets_path
   end
@@ -35,6 +38,7 @@ class PetsController < ApplicationController
 
   def find_pet
     @pet = Pet.find(params[:id])
+    authorize @pet
   end
 
   def pet_params
--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,24 +1,41 @@
 class SheltersController < ApplicationController
+  before_action :find_shelter, only: [:show, :edit, :update, :destroy]
+
   def index
-    @shelters = Shelter.all
+    @shelters = policy_scope(Shelter).order(created_at: :desc)
   end
 
   def show
-    @shelter = Shelter.find(params[:id])
   end
 
   def new
     @shelter = Shelter.new
+    authorize @shelter
   end
 
   def create
     @shelter = Shelter.new(shelter_params)
     @shelter.user = current_user
+    authorize @shelter
     @shelter.save
     redirect_to shelters_path
   end
 
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
   private
+
+  def find_shelter
+    @shelter = Shelter.find(params[:id])
+    authorize @shelter
+  end
 
   def shelter_params
     params.require(:shelter).permit(:name, :address, :phone_number, :user_id)
