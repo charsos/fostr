160dcb7a137fee674f862fe39cc78a2d382eee06
Amber Lowey <lowey45@gmail.com>
2019-03-22 12:41:59 -0600

    started user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
+
   def show
   end
 
@@ -12,7 +14,20 @@ class UsersController < ApplicationController
     redirect_to pets_path
   end
 
+  # def update
+  #   @user.update(user_params)
+  #   redirect_to user_path(@user)
+  # end
+
+  # def edit
+  # end
+
   private
+
+  def find_user
+    @user = User.find(params[:id])
+    # authorize @user
+  end
 
   def user_params
     params.require(:user).permit(:name, :description, :phone_number, :shelter_owner)
